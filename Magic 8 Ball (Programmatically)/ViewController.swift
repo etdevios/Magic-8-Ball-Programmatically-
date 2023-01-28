a5559d1a6b25cb2ad8f190bca2cb8807e4151bfd
Eduard Tokarev <etdevios@gmail.com>
2023-01-28 17:21:22 +0300

    Initial Commit

---
+++ Magic 8 Ball (Programmatically)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball (Programmatically)
+//
+//  Created by Eduard Tokarev on 28.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
